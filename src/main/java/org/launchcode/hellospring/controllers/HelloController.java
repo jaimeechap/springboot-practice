02db8846168d06a1673f99ff3df9b5c6a8c7d983
jaimeechap <jaimee.chapman217@gmail.com>
2022-02-03 14:33:27 -0600

    attempted mvc exercise

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -5,44 +5,50 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
-@RequestMapping("hello")
+@RequestMapping("form")
 public class HelloController {
-    //Handles request at path hello /hello
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello () {
-//        return "Hello, Spring!";
-//    }
-    //lives at /hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye () {
-        return "Goodbye, Spring!";
+    public String name;
+
+    public String getName() {
+        return name;
     }
 
-    // live at hello/hello
-    //Create a handler for requests for form /hello?name=LaunchCode
-    @RequestMapping(method={RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam (@RequestParam String name)  {
-        return "Hello, " + name + "!";
+    public void setName(String name) {
+        this.name = name;
     }
 
-    //handle requests of the form /hello/name
-
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String getLanguage() {
+        return language;
     }
 
-    //lives at hello/form
-    @GetMapping("form")
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String language;
+
     public String helloForm() {
         return "<html>" +
                 "<body>" +
                 "<form action='hello' method='post'>" + //submit a request to hello
                 "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me'>" +
+                "</form>" +
+                "<form action='lang' method='post'>" +
+                "<select name ='language' id='lang-select'>" +
+                "<option value=''>--Please choose a language--</option>" +
+                "<option value='French' >French</option>" +
+                "<option value='English'>English</option>" +
+                "<option value='Spanish'>Spanish</option>" +
+                "<option value='German'>German</option>" +
+                "<option value='Italian'>Italian</option>" +
+                "</select>" +
+                "<input type='submit' value='Greet me' method='post'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
+    }
+@RequestMapping
+    public static String createMessage (String name, String language) {
+        return language + ", " + name;
     }
 }
